77fd8d3d14a067fb733534f05bca68a72e5dfdee
Timo Kluck <tkluck@infty.nl>
2017-07-04 09:16:53 +0200

    Remove debian/ example files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(txdbus):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="txdbus" command="/usr/bin/txdbus"
--- debian/txdbus.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the txdbus package
-#
-0 4	* * *	root	[ -x /usr/bin/txdbus_maintenance ] && /usr/bin/txdbus_maintenance
